cf27f8f7c7d0ec5347752ea472f40d293c63e332
Rita Casiello <rituzka@gmail.com>
2020-05-17 23:09:02 +0200

    Funciona listado de empleados y metodo de calculo de salario.

--- src/com/jobs/application/JobsController.java
+++ src/com/jobs/application/JobsController.java
@@ -1,44 +1,63 @@
 package com.jobs.application;
 
+
+import com.jobs.domain.AbsStaffMember;
 import com.jobs.domain.Employee;
+import com.jobs.domain.Volunteer;
 import com.jobs.persistence.EmployeeRepository;
 
 public class JobsController {
 
-	private EmployeeRepository repository;
+	private EmployeeRepository repository = new EmployeeRepository();
+
 	
-	public JobsController(){
+	
+    public JobsController(){
 		
 	}
 	
 	public void createBossEmployee(String name, String address, String phone, double salaryPerMonth) throws Exception{		
 		Employee boss = new Employee(name, address, phone,  salaryPerMonth, PaymentFactory.createPaymentRateBoss());
+		boss.setTotalPaid(PaymentFactory.createPaymentRateBoss().pay(salaryPerMonth));
 		repository.addMember(boss);
+	
+		
 	}
 	
 	public void createEmployee(String name, String address, String phone, double salaryPerMonth) throws Exception{		
-		Employee boss = new Employee(name, address, phone,  salaryPerMonth, PaymentFactory.createPaymentRateEmployee());
-		repository.addMember(boss);
+		Employee employee = new Employee(name, address, phone,  salaryPerMonth, PaymentFactory.createPaymentRateEmployee());
+		employee.setTotalPaid(PaymentFactory.createPaymentRateEmployee().pay(salaryPerMonth));
+		repository.addMember(employee);
 	}
 
 	public void createManagerEmployee(String name, String address, String phone, double salaryPerMonth) throws Exception{
-		// TODO Auto-generated method stub
+		Employee manager = new Employee(name, address, phone,  salaryPerMonth, PaymentFactory.createPaymentRateManager());
+		manager.setTotalPaid(PaymentFactory.createPaymentRateManager().pay(salaryPerMonth));
+		repository.addMember(manager);
 		
 	}
 
 
 	public void payAllEmployeers() {
-		// TODO Auto-generated method stub
+		for(AbsStaffMember emp: repository.getAllMembers()) {
+		 emp.getTotalPaid();
+		}
 	
 	}
 
 	public String getAllEmployees() {
-		// TODO Auto-generated method stub
-		return null;
+		String allEmployees = "";
+		for (AbsStaffMember emp: repository.getAllMembers()) {
+		allEmployees = "Nombre: "+ emp.getName() +", Dirección: "+emp.getAddress()+", Teléfono: "+emp.getPhone()+", Salario: "+emp.getTotalPaid();
+		
+		System.out.println(allEmployees);
+		}
+		return allEmployees;
 	}
 
-	public void createVolunteer(String string, String string2, String string3) {
-		// TODO Auto-generated method stub
+	public void createVolunteer(String name, String adress, String phone, String description) throws Exception {
+		AbsStaffMember volunteer = new Volunteer(name, adress, phone, description);
+		repository.addMember(volunteer);
 		
 	}
 	
--- src/com/jobs/domain/Volunteer.java
+++ src/com/jobs/domain/Volunteer.java
@@ -2,18 +2,29 @@ package com.jobs.domain;
 
 public class Volunteer extends AbsStaffMember {
 	
+	private String description;
 
 	public Volunteer(String name, String address, String phone, String description) throws Exception {
 		super(name, address, phone);
 		
-		//TODO
+		this.setDescription(description);
 	}
 
 	
 	@Override
 	public void pay() {
-		//TODO
+		totalPaid = 0;
 		
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
 	}
 
 }
